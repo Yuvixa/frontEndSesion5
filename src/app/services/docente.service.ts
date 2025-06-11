0bcddc96af01bf1f1b9e9f98a0445f2d36141e6d
Yuvixa <ycaminazana@gmail.com>
2025-06-10 22:38:34 -0500

    puse registoDocente en el const urlbas

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 
-const urlbase = `${environment.apiUrl}/rest/docente/`;
+const urlbase = `${environment.apiUrl}/rest/docente/registraDocente`;
 
 @Injectable({
   providedIn: 'root',
